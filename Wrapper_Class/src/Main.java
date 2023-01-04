3b6b23989194ed1ddd290d4ee008ee6253ac53c7
shehavishka <2020cs018@stu.ucsc.cmb.ac.lk>
2023-01-04 07:44:24 +0530

    Wrapper class

---
+++ Wrapper_Class/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Wrapper Class data types (reference type)
+        // can use in the collection
+        // those are reference data types there could be some useful methods
+        // disadvantage :-> take more time to access than primitive data types.
+
+        Integer x = 12;
+        Character y = '$';
+        Double d = 12.342;
+
+        if(x==12){
+            System.out.println("thi is a Integer");
+        }
+    }
+}
