ac4e46a405cd35bbef8e4b8c4b3331cf5a3a2e3e
cloby99 <109018087+cloby99@users.noreply.github.com>
2023-01-19 00:45:23 +0530

    JavaBasics

---
+++ OverloadedConstructors/OverloadedConstructors.java
@@ -0,0 +1,26 @@
+public class OverloadedConstructors {
+
+    public static void main(String[] args){
+
+       /* Pizza pizza1 = new Pizza("thick crust", "tomato", "mozzarella");
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza1.bread);
+        System.out.println(pizza1.sauce);
+        System.out.println(pizza1.cheese);
+        System.out.println(pizza1.topping);
+    }
+
+        */
+
+        Pizza pizza2 = new Pizza();
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza2.bread);
+        System.out.println(pizza2.sauce);
+        System.out.println(pizza2.cheese);
+        System.out.println(pizza2.topping);
+}
+        //CHECK***
+
+}
